0640628ee6f9553e62c26262a62d764ae7159167
Elias Perez <eperez@justworks.com>
2024-07-22 09:51:57 -0400

    Refactor update query tests
    Moved update query tests from the main SQL spec file to a dedicated update spec file for better organization and separation of concerns. This change enhances maintainability and clarity by isolating update-specific tests.

---
+++ spec/update_spec.cr
@@ -0,0 +1,29 @@
+require "./spec_helper"
+
+describe Sql::Update do
+  it "creates Update query" do
+    update_query = u.update(:users)
+      .set(name: "'John'", email: "'john@example.com'")
+      .to_sql
+
+    update_query.should eq(
+      <<-SQL.gsub(/\n/, " ").strip
+      UPDATE users SET users.name = 'John', users.email = 'john@example.com'
+      SQL
+    )
+  end
+
+  it "create update where query" do
+    update_query = u.update(:users)
+      .set(name: "'John'", email: "'john@example.com'")
+      .where { users.id == 1 }
+      .to_sql
+
+    update_query.should eq(
+      <<-SQL.gsub(/\n/, " ").strip
+      UPDATE users SET users.name = 'John', users.email = 'john@example.com'
+      WHERE (users.id = 1)
+      SQL
+    )
+  end
+end
