9702ac8164386f9e34386f32be334d4b58190b77
Elias Perez <eperez@justworks.com>
2024-08-04 23:03:13 -0400

    Refactor comparison methods for better maintainability
    Unified comparison methods using method aliases and improved internal logic for handling different value types. This refactoring simplifies the code base, improves readability, and ensures consistent behavior across comparison operators.

--- src/expression/column_builder.cr
+++ src/expression/column_builder.cr
@@ -5,109 +5,73 @@ module Expression
     def initialize(@column : Column)
     end
 
-    def ==(value : DB::Any) : ConditionBuilder
-      compare("=", value)
-    end
-
-    def eq(value : DB::Any) : ConditionBuilder
-      compare("=", value)
-    end
-
-    def <=(value : DB::Any) : ConditionBuilder
-      compare("<=", value)
-    end
-
-    def !=(value : DB::Any) : ConditionBuilder
-      compare("!=", value)
-    end
-
-    def neq(value : DB::Any) : ConditionBuilder
-      compare("!=", value)
-    end
-
-    def <(value : DB::Any) : ConditionBuilder
-      compare("<", value)
-    end
-
-    def >(value : DB::Any) : ConditionBuilder
-      compare(">", value)
-    end
-
-    def >=(value : DB::Any) : ConditionBuilder
-      compare(">=", value)
-    end
-
-    def ==(value : ColumnBuilder | Column) : ConditionBuilder
-      compare("=", value.column)
-    end
-
-    def eq(value : ColumnBuilder | Column) : ConditionBuilder
-      compare("=", value.column)
-    end
-
-    def <=(value : ColumnBuilder | Column) : ConditionBuilder
-      compare("<=", value.column)
-    end
-
-    def !=(value : ColumnBuilder | Column) : ConditionBuilder
-      compare("!=", value.column)
-    end
-
-    def neq(value : ColumnBuilder | Column) : ConditionBuilder
-      compare("!=", value.column)
-    end
-
-    def <(value : ColumnBuilder | Column) : ConditionBuilder
-      compare("<", value.column)
-    end
-
-    def >(value : ColumnBuilder | Column) : ConditionBuilder
-      compare(">", value.column)
-    end
-
-    def >=(value : ColumnBuilder | Column) : ConditionBuilder
-      compare(">=", value.column)
-    end
-
-    def in(items : Array)
-      ConditionBuilder.new(InCondition.new(@column, items))
-    end
-
-    def in(sub_query : Query)
-      ConditionBuilder.new(InSelect.new(@column, sub_query.build))
-    end
-
-    def not_in(values : Array(DB::Anyy))
-      ConditionBuilder.new(Not.new(InCondition.new(@column, values)))
-    end
-
-    def not_in(sub_query : Query)
-      ConditionBuilder.new(InSelect.new(Not.new(@column, sub_query.build)))
-    end
-
-    def like(pattern : String)
-      ConditionBuilder.new(Like.new(@column, pattern))
-    end
-
-    def not_like(pattern : String)
-      ConditionBuilder.new(NotLike.new(@column, pattern))
-    end
-
-    def null
-      ConditionBuilder.new(IsNull.new(@column))
-    end
-
-    def not_null
-      ConditionBuilder.new(IsNotNull.new(@column))
-    end
-
-    private def compare(operator : String, value : Column | CompareCondition) : ConditionBuilder
+    private def compare(operator : String, value : Column | ConditionBuilder)
       ConditionBuilder.new(CompareCondition.new(@column, operator, value))
     end
 
-    private def compare(operator : String, value : DB::Any) : ConditionBuilder
+    private def compare(operator : String, value : DB::Any)
       @column.column.validate!(value)
       ConditionBuilder.new(Compare.new(@column, operator, value))
+    end
+
+    # Redefine comparison operators using method aliases
+    {% for operator, method_name in {
+                                      "==" => :eq,
+                                      "!=" => :neq,
+                                      "<=" => :lte,
+                                      "<"  => :lt,
+                                      ">"  => :gt,
+                                      ">=" => :gte,
+                                    } %}
+        {% optr = (operator == "==") ? "=" : operator %}
+
+      def {{operator.id}}(value : DB::Any) : ConditionBuilder
+        compare("{{optr.id}}", value)
+      end
+
+      def {{method_name.id}}(value : DB::Any) : ConditionBuilder
+        compare("{{optr.id}}", value)
+      end
+
+      def {{operator.id}}(value : ColumnBuilder | Column) : ConditionBuilder
+        compare("{{optr.id}}", value.column)
+      end
+
+      def {{method_name.id}}(value : ColumnBuilder | Column) : ConditionBuilder
+        compare("{{optr.id}}", value.column)
+      end
+    {% end %}
+
+    def in(items : Array(DB::Any)) : ConditionBuilder
+      ConditionBuilder.new(InCondition.new(@column, items))
+    end
+
+    def in(sub_query : Query) : ConditionBuilder
+      ConditionBuilder.new(InSelect.new(@column, sub_query.build))
+    end
+
+    def not_in(values : Array(DB::Any)) : ConditionBuilder
+      ConditionBuilder.new(Not.new(InCondition.new(@column, values)))
+    end
+
+    def not_in(sub_query : Query) : ConditionBuilder
+      ConditionBuilder.new(Not.new(InSelect.new(@column, sub_query.build)))
+    end
+
+    def like(pattern : String) : ConditionBuilder
+      ConditionBuilder.new(Like.new(@column, pattern))
+    end
+
+    def not_like(pattern : String) : ConditionBuilder
+      ConditionBuilder.new(NotLike.new(@column, pattern))
+    end
+
+    def null : ConditionBuilder
+      ConditionBuilder.new(IsNull.new(@column))
+    end
+
+    def not_null : ConditionBuilder
+      ConditionBuilder.new(IsNotNull.new(@column))
     end
   end
 end
