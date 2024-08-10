d3eba6f6fa087ee26f22e8048a7755b126f06147
Elias Perez <eperez@justworks.com>
2024-08-10 14:43:51 -0400

    Add comprehensive documentation for migrations
    Enhanced the `migrations.cr` file by adding extensive documentation on the usage of the `Migration` and `Migrator` classes. This includes examples for creating, applying, rolling back, redoing migrations, and listing migration statuses. These detailed comments and examples aim to improve the clarity of the migration process for developers and ensure proper usage of migration functionality.
    
    No functional changes or bug fixes were made as part of this documentation enhancement.

--- src/migrations.cr
+++ src/migrations.cr
@@ -2,12 +2,101 @@ require "tallboy"
 require "colorize"
 
 module Cql
+  # Migrations are used to manage changes to the database schema over time.
+  # Each migration is a subclass of `Migration` and must implement the `up` and `down` methods.
+  #
+  # The `up` method is used to apply the migration, while the `down` method is used to rollback the migration.
+  # Migrations are executed in their version order defined.
+  # The `Migrator` class is used to manage migrations and provides methods to apply, rollback, and redo migrations.
+  # The `Migrator` class also provides methods to list applied and pending migrations.
+  # ## Example Creating a new migration
+  #
+  # ```
+  # class CreateUsersTable < Cql::Migration
+  #   self.version = 1_i64
+  #
+  #   def up
+  #     schema.table :users do
+  #       primary :id, Int64
+  #       column :name, String
+  #       column :email, String
+  #     end
+  #   end
+  #
+  #   def down
+  #     schema.drop_table :users
+  #   end
+  # end
+  # ```
+  #
+  # ## Example Applying migrations
+  # ```
+  # schema = Cql::Schema.build(:northwind, "sqlite3://db.sqlite3") do |s|
+  #   s.create_table :schema_migrations do
+  #     primary :id, Int32
+  #     column :name, String
+  #     column :version, Int64, index: true, unique: true
+  #     timestamps
+  #   end
+  # end
+  # migrator = Cql::Migrator.new(schema)
+  # migrator.up
+  # ```
+  #
+  # ## Example Rolling back migrations
+  # ```
+  # migrator.down
+  # ```
+  #
+  # ## Example Redoing migrations
+  # ```
+  # migrator.redo
+  # ```
+  #
+  # ## Example Rolling back to a specific version
+  # ```
+  # migrator.down_to(1_i64)
+  # ```
+  #
+  # ## Example Applying to a specific version
+  # ```
+  # migrator.up_to(1_i64)
+  # ```
+  #
+  # ## Example Listing applied migrations
+  # ```
+  # migrator.print_applied_migrations
+  # ```
+  #
+  # ## Example Listing pending migrations
+  # ```
+  # migrator.print_pending_migrations
+  # ```
+  #
+  # ## Example Listing rolled back migrations
+  # ```
+  # migrator.print_rolled_back_migrations
+  # ```
+  #
+  # ## Example Listing the last migration
+  # ```
+  # migrator.last
+  # ```
+  #
   abstract class Migration
     getter schema : Schema
 
     macro inherited
       class_getter version : Int64 = 0_i64
 
+      # Sets the migration version.
+      # - **@param** number [Int64] the migration version
+      # ## Example Setting the migration version
+      # ```
+      # class CreateUsersTable < Cql::Migration
+      #  self.version = 1_i64
+      # end
+      # ```
       def self.version=(number : Int64)
         @@version = number
       end
@@ -20,9 +109,39 @@ module Cql
     abstract def down
   end
 
+  # The `Migrator` class is used to manage migrations and provides methods to apply,
+  # rollback, and redo migrations.
+  # The `Migrator` class also provides methods to list applied and pending migrations.
+  # ## Example Creating a new migrator
+  # ```
+  # schema = Cql::Schema.build(:northwind, "sqlite3://db.sqlite3") do |s|
+  #   s.create_table :schema_migrations do
+  #     primary :id, Int32
+  #     column :name, String
+  #     column :version, Int64, index: true, unique: true
+  #     timestamps
+  #   end
+  # end
+  # migrator = Cql::Migrator.new(schema)
+  # ```
+  #
+  # ## Example Applying migrations
+  # ```
+  # migrator.up
+  # ```
   class Migrator
     Log = ::Log.for(self)
 
+    # Represents a migration record.
+    # @field id [Int64] the migration record id
+    # @field name [String] the migration name
+    # @field version [Int64] the migration version
+    # @field created_at [Time] the creation time
+    # @field updated_at [Time] the update time
+    # ## Example Creating a migration record
+    # ```
+    # record = Cql::MigrationRecord.new(0_i64, "CreateUsersTable", 1_i64)
+    # ```
     class MigrationRecord
       include DB::Serializable
 
@@ -51,6 +170,13 @@ module Cql
       @repo = Repository(MigrationRecord).new(schema, :schema_migrations)
     end
 
+    # Applies the pending migrations.
+    #  - **@param** steps [Int32] the number of migrations to apply (default: all)
+    # ## Example Applying migrations
+    # ```
+    # migrator.up
+    # ```
+    #
     def up(steps : Int32 = Migrator.migrations.size)
       sorted_migrations[0, steps].each do |migration_class|
         unless migration_applied?(migration_class.version)
@@ -61,6 +187,12 @@ module Cql
       print_applied_migrations
     end
 
+    # Rolls back the last migration.
+    # - **@param** steps [Int32] the number of migrations to roll back (default: 1)
+    # ## Example Rolling back migrations
+    # ```
+    # migrator.down
+    # ```
     def down(steps : Int32 = Migrator.migrations.size)
       sorted_migrations.reverse[0, steps].each do |migration_class|
         if migration_applied?(migration_class.version)
@@ -71,47 +203,106 @@ module Cql
       print_rolled_back_migrations(sorted_migrations.reverse[0, steps])
     end
 
+    # Rolls back the last migration.
+    # - **@param** steps [Int32] the number of migrations to roll back (default: 1)
+    # ## Example Rolling back migrations
+    # ```
+    # migrator.rollback
+    # ```
+    #
     def rollback(steps : Int32 = 1)
       down(steps)
     end
 
+    # Redoes the last migration.
+    # ## Example Redoing migrations
+    # ```
+    # migrator.redo
+    # ```
     def redo
       rollback
       up
     end
 
+    # Returns the last migration.
+    # ## Example Listing the last migration
+    # ```
+    # migrator.last
+    # ```
+    # @return [Migration.class | Nil]
     def last : Migration.class | Nil
       Migrator.migrations.find { |m| m.version == repo.last.version }
     rescue DB::NoResultsError
       nil
     end
 
+    # Rolls back to a specific migration version.
+    # - **@param** version [Int64] the version to roll back to
+    # ## Example Rolling back to a specific version
+    # ```
+    # migrator.down_to(1_i64)
+    # ```
     def down_to(version : Int64)
       index = sorted_migrations.index { |m| m.version == version }
       down(index ? index + 1 : 0) if index
     end
 
+    # Applies migrations up to a specific version.
+    # - **@param** version [Int64] the version to apply up to
+    # ## Example Applying to a specific version
+    # ```
+    # migrator.up_to(1_i64)
+    # ```
     def up_to(version : Int64)
       index = sorted_migrations.index { |m| m.version == version }
       up(index ? index + 1 : 0) if index
     end
 
+    # Prints the rolled back migrations.
+    # - **@param** m [Array(Migration.class)] the migrations to print
+    # - **@return** [Nil]
+    # ## Example Listing rolled back migrations
+    # ```
+    # migrator.print_rolled_back_migrations
+    # ```
     def print_rolled_back_migrations(m : Array(Migration.class))
       print_table(m.map { |migration| build_migration_record(migration) }, "✗".colorize.red.to_s)
     end
 
+    # Prints the applied migrations.
+    # ## Example Listing applied migrations
+    # ```
+    # migrator.print_applied_migrations
+    # ```
     def print_applied_migrations
       print_table(applied_migrations)
     end
 
+    # Prints the pending migrations.
+    # ## Example Listing pending migrations
+    # ```
+    # migrator.print_pending_migrations
+    # ```
     def print_pending_migrations
       print_table(pending_migrations, "⏱".colorize.yellow.to_s)
     end
 
+    # Returns the pending migrations.
+    # - **@return** [Array(MigrationRecord)]
+    # ## Example Listing pending migrations
+    # ```
+    # migrator.pending_migrations
+    # ```
     def pending_migrations : Array(MigrationRecord)
       sorted_migrations.reject { |m| migration_applied?(m.version) }.map { |m| build_migration_record(m) }
     end
 
+    # Returns the applied migrations.
+    # - **@return** [Array(MigrationRecord)]
+    # ## Example Listing applied migrations
+    # ```
+    # migrator.applied_migrations
+    # ```
     def applied_migrations : Array(MigrationRecord)
       repo.all
     end
